2327deb767729bd9aa7ee75d9c165e3cdce12e74
KarryCharon <karrycharon@qq.com>
2024-07-17 10:13:57 +0800

    add cldm ldm lib

---
+++ ToonCrafter/ldm/modules/image_degradation/__init__.py
@@ -0,0 +1,2 @@
+from ToonCrafter.ldm.modules.image_degradation.bsrgan import degradation_bsrgan_variant as degradation_fn_bsr
+from ToonCrafter.ldm.modules.image_degradation.bsrgan_light import degradation_bsrgan_variant as degradation_fn_bsr_light
